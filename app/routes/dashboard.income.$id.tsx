b8b59a167001f0266a6ccf0d4497d5354bcadd1f
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2024-03-04 14:30:15 -0800

    Handling errors in Income Page

--- app/routes/dashboard.income.$id.tsx
+++ app/routes/dashboard.income.$id.tsx
@@ -1,9 +1,17 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
-import { useActionData, useLoaderData, useNavigation } from '@remix-run/react';
+import {
+  isRouteErrorResponse,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+  useParams,
+  useRouteError,
+} from '@remix-run/react';
 
 import { Button } from '~/components/buttons';
 import { Form, Input, Textarea } from '~/components/forms';
+import { H2 } from '~/components/headings';
 import { FloatingActionLink } from '~/components/links';
 import { db } from '~/modules/db.server';
 
@@ -34,6 +42,26 @@ export async function action({ params, request }: ActionFunctionArgs) {
   }
 
   throw new Response('Bad request', { status: 400 });
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  const { id } = useParams();
+  let heading = 'Something went wrong';
+  let message = `Apologies, something went wrong on our end, please try again.`;
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    heading = 'Invoice not found';
+    message = `Apologies, the invoice with the id ${id} cannot be found.`;
+  }
+  return (
+    <>
+      <div className="w-full m-auto lg:max-w-3xl flex flex-col items-center justify-center gap-5">
+        <H2>{heading}</H2>
+        <p>{message}</p>
+      </div>
+      <FloatingActionLink to="/dashboard/income/">Add invoice</FloatingActionLink>
+    </>
+  );
 }
 
 export async function updateInvoice(formData: FormData, id: string) {
