d9e2803d02febfb7d4a82a4377f5b4de4d9f03ae
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2024-03-04 14:23:50 -0800

    Handling errors in Expenses Page

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,8 @@
 import type { LinksFunction, MetaFunction } from '@remix-run/node';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useRouteError } from '@remix-run/react';
 
+import { H1 } from './components/headings';
+import { ButtonLink } from './components/links';
 import { PageTransitionProgressBar } from './components/progress';
 import tailwindCSS from './styles/tailwind.css';
 
@@ -10,22 +12,49 @@ export const meta: MetaFunction = () => {
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: tailwindCSS }];
 
-export default function Component() {
+export function ErrorBoundary() {
+  const error = useRouteError();
+  let errorMessage = error instanceof Error ? error.message : null;
+  return (
+    <Document>
+      <H1>Unexpected Error</H1>
+      <p>We are very sorry. An unexpected error occurred. Please try again or contact us if the problem persists.</p>
+      {errorMessage && (
+        <div className="border-4 border-red-500 p-10">
+          <p>Error message: {errorMessage}</p>
+        </div>
+      )}
+      <ButtonLink to="/" isPrimary>
+        Back to homepage
+      </ButtonLink>
+    </Document>
+  );
+}
+
+function Document({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body className="bg-background dark:bg-darkBackground text-lg text-text dark:text-darkText">
         <PageTransitionProgressBar />
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function Component() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
   );
 }
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,7 +1,9 @@
-import { json } from '@remix-run/node';
-import { Link as RemixLink, Outlet, useLoaderData, useLocation } from '@remix-run/react';
+import type { Expense, Invoice } from '@prisma/client';
+import { json, SerializeFrom } from '@remix-run/node';
+import { Link as RemixLink, Outlet, useLoaderData, useLocation, useRouteError } from '@remix-run/react';
 
 import { Container } from '~/components/containers';
+import { H1 } from '~/components/headings';
 import { NavLink } from '~/components/links';
 import { db } from '~/modules/db.server';
 
@@ -13,13 +15,39 @@ export async function loader() {
   return json({ firstExpense, firstInvoice });
 }
 
-export default function Component() {
-  const { firstExpense, firstInvoice } = useLoaderData<typeof loader>();
+export function ErrorBoundary() {
+  const error = useRouteError();
+  const errorMessage = error instanceof Error && error.message;
+  return (
+    <Layout firstExpense={null} firstInvoice={null}>
+      <Container className="p-5 lg:p-20 flex flex-col gap-5">
+        <H1>Unexpected Error</H1>
+        <p>We are very sorry. An unexpected error occurred. Please try again or contact us if the problem persists.</p>
+        {errorMessage && (
+          <div className="border-4 border-red-500 p-10">
+            <p>Error message: {error.message}</p>
+          </div>
+        )}
+      </Container>
+    </Layout>
+  );
+}
 
+type LayoutProps = {
+  children: React.ReactNode;
+  //Remix's SerializeFrom type transforms the Expense and Invoice types into
+  //their serialized version. This is necessary as the data travels over the
+  //network, serialized as JSON.
+  //For instance, createdAt field is of the Date type on the server but serialized
+  //as string once accessed via useLoaderData.
+  firstExpense: SerializeFrom<Expense> | null;
+  firstInvoice: SerializeFrom<Invoice> | null;
+};
+
+function Layout({ firstExpense, firstInvoice, children }: LayoutProps) {
   //Remix's useLocation hook lets us access a global location object with information
   //about the current URL.
   const location = useLocation();
-
   return (
     <>
       <header>
@@ -35,6 +63,8 @@ export default function Component() {
             </ul>
             <ul className="mt-10 w-full flex flex-row gap-5">
               <li className="ml-auto">
+                {/* if prefetch is set to render, then the loader data and assets for the link are fetched
+                once this link is rendered on the page. */}
                 {/* If prefetch is set to intent, then Remix starts prefetching once the user focuses or 
                 hovers over the link */}
                 <NavLink
@@ -58,9 +88,17 @@ export default function Component() {
           </nav>
         </Container>
       </header>
-      <main className="p-4 w-full flex justify-center items-center">
-        <Outlet />
-      </main>
+      <main className="p-4 w-full flex justify-center items-center">{children}</main>
     </>
+  );
+}
+
+export default function Component() {
+  const { firstExpense, firstInvoice } = useLoaderData<typeof loader>();
+
+  return (
+    <Layout firstExpense={firstExpense} firstInvoice={firstInvoice}>
+      <Outlet />
+    </Layout>
   );
 }
