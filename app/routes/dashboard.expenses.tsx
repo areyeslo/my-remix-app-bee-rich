b09d0267ef0e0ec2baeb6e44b813ee1d507314a5
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2024-03-12 16:24:36 -0700

    Basic search by title for expenses and income.

--- app/routes/dashboard.expenses.tsx
+++ app/routes/dashboard.expenses.tsx
@@ -1,7 +1,9 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { Outlet, useLoaderData, useNavigation, useParams } from '@remix-run/react';
+import { Form, Outlet, useLoaderData, useLocation, useNavigation, useParams, useSearchParams } from '@remix-run/react';
 import clsx from 'clsx';
 
+import { Input } from '~/components/forms';
 import { H1 } from '~/components/headings';
 import { ListLinkItem } from '~/components/links';
 import { db } from '~/modules/db.server';
@@ -16,20 +18,34 @@ import { db } from '~/modules/db.server';
 //Remix refreshes all loader data by re-fetching from all active loader functions after
 //executing an action function, just like a full-page reload would do on a HTML form
 //submission.
-export async function loader() {
-  const expenses = await db.expense.findMany({ orderBy: { createdAt: 'desc' } });
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const searchString = url.searchParams.get('q');
+
+  const expenses = await db.expense.findMany({
+    orderBy: { createdAt: 'desc' },
+    where: { title: { contains: searchString ? searchString : '' } },
+  });
   console.log(`expenses retrieved: ${expenses}`);
   return json(expenses);
 }
 
 export default function Component() {
   const navigation = useNavigation();
+  const location = useLocation();
   console.log(navigation.state);
 
   //Use the useLoaderData hook to access the loader data of the same route module.
   //Remix executes loader functions first and then renders the React application on the server.
   //useLoaderData returns the data of the route module that the hook is called in.
   const expenses = useLoaderData<typeof loader>();
+
+  // The advantage of search parameters over React state is that they can be accessed on
+  // server by reading from the request  URL. Search parameters persist on full-page reoloads
+  // nand work with the browser's back and forward buttons.
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get('q') || '';
+  console.log(`searchQuery: ${searchQuery}`);
 
   //useParam hook to access the id route parameter of the expense details page.
   const { id } = useParams();
@@ -40,6 +56,10 @@ export default function Component() {
       <div className="mt-10 w-full flex flex-col-reverse lg:flex-row">
         <section className="lg:p-8 w-full lg:max-w-2xl">
           <h2 className="sr-only">All expenses</h2>
+          {/* include action to point to the current URL path */}
+          <Form method="GET" action={location.pathname}>
+            <Input name="q" type="search" label="Search by title" defaultValue={searchQuery} />
+          </Form>
           <ul className="flex flex-col">
             {expenses.map((expense) => (
               <ListLinkItem
--- app/routes/dashboard.income.tsx
+++ app/routes/dashboard.income.tsx
@@ -1,23 +1,34 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { Outlet, useLoaderData, useNavigation, useParams } from '@remix-run/react';
+import { Form, Outlet, useLoaderData, useLocation, useNavigation, useParams, useSearchParams } from '@remix-run/react';
 import clsx from 'clsx';
 
+import { Input } from '~/components/forms';
 import { H1 } from '~/components/headings';
 import { ListLinkItem } from '~/components/links';
 import { db } from '~/modules/db.server';
 
-export async function loader() {
-  const invoice = await db.invoice.findMany({ orderBy: { createdAt: 'desc' } });
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const searchString = url.searchParams.get('q');
+
+  const invoice = await db.invoice.findMany({
+    orderBy: { createdAt: 'desc' },
+    where: { title: { contains: searchString ? searchString : '' } },
+  });
   console.log(`income retrieved: ${invoice}`);
   return json(invoice);
 }
 
 export default function Component() {
   const navigation = useNavigation();
+  const location = useLocation();
   console.log(navigation.state);
 
   const invoices = useLoaderData<typeof loader>();
 
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get('q') || '';
   const { id } = useParams();
 
   return (
@@ -26,6 +37,9 @@ export default function Component() {
       <div className="mt-10 w-full flex flex-col-reverse lg:flex-row">
         <section className="lg:p-8 w-full lg:max-w-2xl">
           <h2 className="sr-only">Total income</h2>
+          <Form method="GET" action={location.pathname}>
+            <Input name="q" type="search" label="Search by title" defaultValue={searchQuery} />
+          </Form>
           <ul className="flex flex-col">
             {invoices.map((invoice) => (
               <ListLinkItem
